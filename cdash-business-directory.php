20ce77bca19e6af891c9c7b5d9a0ce7633b11aa3
Gwendydd <Gwendydd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-14 06:13:54 +0000

    updating version number correctly
    git-svn-id: https://plugins.svn.wordpress.org/chamber-dashboard-business-directory/trunk@965569 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cdash-business-directory.php
+++ cdash-business-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: Chamber Dashboard Business Directory
 Plugin URI: http://chamberdashboard.com
 Description: Create a database of the businesses in your chamber of commerce
-Version: 1.0
+Version: 1.1
 Author: Morgan Kay
 Author URI: http://wpalchemists.com
 */
