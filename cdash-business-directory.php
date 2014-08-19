59237e14bd5e2a11e43cee0b744c0c2260613d11
Gwendydd <Gwendydd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-19 03:28:42 +0000

    tagging version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/chamber-dashboard-business-directory/tags/1.2@967992 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cdash-business-directory.php
+++ cdash-business-directory.php
@@ -3,7 +3,7 @@
 Plugin Name: Chamber Dashboard Business Directory
 Plugin URI: http://chamberdashboard.com
 Description: Create a database of the businesses in your chamber of commerce
-Version: 0.1
+Version: 1.1
 Author: Morgan Kay
 Author URI: http://wpalchemists.com
 */
@@ -30,7 +30,7 @@ Author URI: http://wpalchemists.com
 // ------------------------------------------------------------------------
 
 
-function requires_wordpress_version() {
+function cdash_requires_wordpress_version() {
 	global $wp_version;
 	$plugin = plugin_basename( __FILE__ );
 	$plugin_data = get_plugin_data( __FILE__, false );
@@ -42,13 +42,10 @@ function requires_wordpress_version() {
 		}
 	}
 }
-add_action( 'admin_init', 'requires_wordpress_version' );
+add_action( 'admin_init', 'cdash_requires_wordpress_version' );
 
 // ------------------------------------------------------------------------
 // REGISTER HOOKS & CALLBACK FUNCTIONS:
-// ------------------------------------------------------------------------
-// HOOKS TO SETUP DEFAULT PLUGIN OPTIONS, HANDLE CLEAN-UP OF OPTIONS WHEN
-// PLUGIN IS DEACTIVATED AND DELETED, INITIALISE PLUGIN, ADD OPTIONS PAGE.
 // ------------------------------------------------------------------------
 
 // Set-up Action and Filter Hooks
@@ -67,6 +64,10 @@ function cdash_language_init() {
   load_plugin_textdomain( 'cdash', false, 'cdash-business-directory/languages' );
 }
 add_action('init', 'cdash_language_init');
+
+// ------------------------------------------------------------------------
+// SET UP CUSTOM POST TYPES AND TAXONOMIES
+// ------------------------------------------------------------------------
 
 // Register Custom Taxonomy - Business Cateogory
 function cdash_register_taxonomy_business_category() {
@@ -94,7 +95,7 @@ function cdash_register_taxonomy_business_category() {
 		'public'                     => true,
 		'show_ui'                    => true,
 		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
+		'show_in_nav_menus'          => false,
 		'show_tagcloud'              => true,
 	);
 	register_taxonomy( 'business_category', array( 'business' ), $args );
@@ -129,7 +130,7 @@ function cdash_register_taxonomy_membership_level() {
 		'public'                     => true,
 		'show_ui'                    => true,
 		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
+		'show_in_nav_menus'          => false,
 		'show_tagcloud'              => true,
 	);
 	register_taxonomy( 'membership_level', array( 'business' ), $args );
@@ -184,7 +185,10 @@ function cdash_register_cpt_business() {
 add_action( 'init', 'cdash_register_cpt_business', 0 );
 
 
-//Create metaboxes
+// ------------------------------------------------------------------------
+// SET UP METABOXES
+// ------------------------------------------------------------------------
+
 include_once 'wpalchemy/MetaBox.php';
 include_once 'wpalchemy/MediaAccess.php';
 define( 'MYPLUGINNAME_PATH', plugin_dir_path(__FILE__) );
@@ -234,7 +238,24 @@ $busnotes_metabox = new WPAlchemy_MetaBox(array
     'prefix' => '_cdash_'
 ));
 
-/* TODO - make a metabox for custom fields */
+$options = get_option('cdash_directory_options');
+if(!empty($options['bus_custom'])) {
+	// Create metabox for custom fields
+	$custom_metabox = new WPAlchemy_MetaBox(array
+	(
+	    'id' => 'custom_meta',
+	    'title' => 'Custom Fields',
+	    'types' => array('business'),
+	    'template' => MYPLUGINNAME_PATH . '/wpalchemy/buscustom.php',
+	    'mode' => WPALCHEMY_MODE_EXTRACT,
+	    'prefix' => '_cdash_'
+	));
+}
+
+
+// ------------------------------------------------------------------------
+// SINGLE BUSINESS VIEW
+// ------------------------------------------------------------------------
 
 // Enqueue stylesheet for single businesses
 function cdash_single_business_style() {
@@ -277,27 +298,31 @@ function cdash_single_business($content) {
 		if (($options['sv_memberlevel']) == "1") { 
 			$id = get_the_id();
 			$levels = get_the_terms( $id, 'membership_level');
-			$business_content .= "<p class='membership'><span>Membership Level:</span>&nbsp;";
-			$i = 1;
-			foreach($levels as $level) {
-				if($i !== 1) {
-					$business_content .= ",&nbsp;";
+			if($levels) {
+				$business_content .= "<p class='membership'><span>Membership Level:</span>&nbsp;";
+				$i = 1;
+				foreach($levels as $level) {
+					if($i !== 1) {
+						$business_content .= ",&nbsp;";
+					}
+					$business_content .= $level->name;
+					$i++;
 				}
-				$business_content .= $level->name;
-				$i++;
 			}
 		}
 		if (($options['sv_category']) == "1") { 
 			$id = get_the_id();
-			$levels = get_the_terms( $id, 'business_category');
-			$business_content .= "<p class='categories'><span>Categories:</span>&nbsp;";
-			$i = 1;
-			foreach($levels as $level) {
-				if($i !== 1) {
-					$business_content .= ",&nbsp;";
+			$buscats = get_the_terms( $id, 'business_category');
+			if($buscats) {
+				$business_content .= "<p class='categories'><span>Categories:</span>&nbsp;";
+				$i = 1;
+				foreach($buscats as $buscat) {
+					if($i !== 1) {
+						$business_content .= ",&nbsp;";
+					}
+					$business_content .= $buscat->name;
+					$i++;
 				}
-				$business_content .= $level->name;
-				$i++;
 			}
 		}
 		$locations = $contactmeta['location'];
@@ -361,7 +386,23 @@ function cdash_single_business($content) {
 						}
 					$business_content .= "</p>";
 				}
+			$business_content .= "</div>";
 			}
+		}
+		if($options['bus_custom']) {
+			$customfields = $options['bus_custom'];
+			foreach($customfields as $field) { 
+				if($field['display_single'] !== "yes") {
+					continue;
+				} else {
+					global $custom_metabox;
+					$custommeta = $custom_metabox->the_meta();
+					$business_content .= "<p><strong>" . $field['name'] . ":</strong>&nbsp;" . $custommeta[$field['name']] . "</p>";
+				}
+			}
+		}
+		if ($options['sv_map'] == "1" ) {
+			$business_content .= "<div id='map-canvas' style='width: 100%; height: 300px; margin: 20px 0;'></div>";
 		}
 		$business_content .= "</div>";
 	$content = $business_content;
@@ -373,7 +414,9 @@ function cdash_single_business($content) {
 add_filter('the_content', 'cdash_single_business');
 
 
-// Create shortcode for displaying business directory
+// ------------------------------------------------------------------------
+// BUSINESS DIRECTORY SHORTCODE
+// ------------------------------------------------------------------------
 
 function cdash_business_directory_shortcode( $atts ) {
 	// Set our default attributes
@@ -542,6 +585,19 @@ function cdash_business_directory_shortcode( $atts ) {
 						}
 				  	}
 			  	}
+			  	$options = get_option('cdash_directory_options');
+			  	if($options['bus_custom']) {
+					$customfields = $options['bus_custom'];
+					foreach($customfields as $field) { 
+						if($field['display_dir'] !== "yes") {
+							continue;
+						} else {
+							global $custom_metabox;
+							$custommeta = $custom_metabox->the_meta();
+							$business_list .= "<p><strong>" . $field['name'] . ":</strong>&nbsp;" . $custommeta[$field['name']] . "</p>";
+						}
+					}
+				}
 			  	$business_list .= "</div>";
 			endwhile;
 
@@ -566,3 +622,192 @@ function cdash_business_directory_shortcode( $atts ) {
 	wp_reset_postdata();
 }
 add_shortcode( 'business_directory', 'cdash_business_directory_shortcode' );
+
+// Create shortcode for displaying map of businesses
+function cdash_business_map_shortcode( $atts ) {
+	// Set our default attributes
+	extract( shortcode_atts(
+		array(
+			'category' => '', // options: slug of any category
+			'level' => '', // options: sluf of any membership level
+			'single_link' => 'yes', // options: yes, no
+			'perpage' => '-1', // options: any number
+		), $atts )
+	);
+
+	$args = array( 
+		'post_type' => 'business',
+		'posts_per_page' => $perpage, 
+	    'business_category' => $category,	
+	    'membership_level' => $level,								 
+	);
+
+	$mapquery = new WP_Query( $args );
+	$business_map .= "<div id='map-canvas' style='width: 100%; height: 500px;'></div>";
+	$business_map .= "<script type='text/javascript' src='https://maps.googleapis.com/maps/api/js?key=AIzaSyDF-0o3jloBzdzSx7rMlevwNSOyvq0G35A&sensor=false'></script>";
+	$business_map .= "<script type='text/javascript'>";
+	$business_map .= "function initialize() {
+				var locations = [";
+
+	// The Loop
+	if ( $mapquery->have_posts() ) :
+		while ( $mapquery->have_posts() ) : $mapquery->the_post();
+			global $buscontact_metabox;
+			$contactmeta = $buscontact_metabox->the_meta();
+			$locations = $contactmeta['location'];
+			foreach($locations as $location) {
+		    	$rawaddress = $location['address'] . $location['city'] . $location['state'] . $location['zip'];
+				$address = str_replace(' ', '+', $rawaddress);
+
+				$json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=$address");
+				$json = json_decode($json);
+				$lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+				$long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'}; 
+				// TODO - let people upload different icons for each category
+				$icon = plugins_url() . '/cdash-business-directory/images/map_marker.png'; 
+				if($single_link == "yes") {
+					$business_map .= "['<div class=\x22business\x22 style=\x22width: 150px; height: 150px;\x22><h5><a href=\x22" . get_the_permalink() . "\x22>" . get_the_title() . "</a></h5> " . $location['address'] . "<br />" . $location['city'] . ", " . $location['state'] . "&nbsp;" . $location['zip'] . "</div>', " . $lat . ", " . $long . ", '" . $icon . "'],";
+				} else {
+					$business_map .= "['<div class=\x22business\x22 style=\x22width: 150px; height: 150px;\x22><h5>" . get_the_title() . "</h5> " . $location['address'] . "<br />" . $location['city'] . ", " . $location['state'] . "&nbsp;" . $location['zip'] . "</div>', " . $lat . ", " . $long . ", '" . $icon . "'],";
+				}
+			}
+		endwhile;
+	endif;
+
+	$business_map .= "];
+
+					var bounds = new google.maps.LatLngBounds();
+					var mapOptions = {
+					    // zoom: 13,
+					}
+					var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+					var infowindow = new google.maps.InfoWindow();
+					var marker, i;
+
+				    for (i = 0; i < locations.length; i++) {  
+				    	marker = new google.maps.Marker({
+				        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+				        map: map,
+				        icon: locations[i][3]
+				    	});
+
+						bounds.extend(marker.position);
+
+						google.maps.event.addListener(marker, 'click', (function(marker, i) {
+						    return function() {
+						        infowindow.setContent(locations[i][0]);
+						        infowindow.open(map, marker);
+						    }
+						})(marker, i));
+
+						map.fitBounds(bounds);
+
+					}
+				}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
+
+		</script>";
+
+	return $business_map;
+	wp_reset_postdata();
+}
+add_shortcode( 'business_map', 'cdash_business_map_shortcode' );
+
+// Add map to single business view
+function cdash_single_business_map() {
+	$options = get_option('cdash_directory_options');
+	if( is_singular('business') && $options['sv_map'] == "1" ) { 
+		global $buscontact_metabox;
+		$contactmeta = $buscontact_metabox->the_meta();
+		$locations = $contactmeta['location']; ?>
+		<script type="text/javascript"
+			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDF-0o3jloBzdzSx7rMlevwNSOyvq0G35A&sensor=false">
+		</script>
+		<script type="text/javascript">
+
+			function initialize() {
+				var locations = [
+					<?php 
+					foreach($locations as $location) {
+				    	$rawaddress = $location['address'] . $location['city'] . $location['state'] . $location['zip'];
+						$address = str_replace(' ', '+', $rawaddress);
+
+						$json = file_get_contents("http://maps.google.com/maps/api/geocode/json?address=$address");
+						$json = json_decode($json);
+						$lat = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lat'};
+						$long = $json->{'results'}[0]->{'geometry'}->{'location'}->{'lng'}; 
+						// TODO - let people upload different icons for each category
+						$icon = plugins_url() . '/cdash-business-directory/images/map_marker.png'; ?>
+						['<div class="business" style="width: 150px; height: 150px;"><h5><?php echo $location["altname"]; ?></h5><?php echo $location["address"]; ?><br /><?php echo $location["city"]; ?>, <?php echo $location["state"]; ?> <?php echo $location["zip"]; ?></div>', <?php echo $lat; ?>, <?php echo $long; ?>, '<?php echo $icon; ?>'],
+						
+					<?php } ?>
+
+					];
+
+					var bounds = new google.maps.LatLngBounds();
+					var mapOptions = {
+					    // zoom: 13,
+					}
+					var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+					var infowindow = new google.maps.InfoWindow();
+					var marker, i;
+
+				    for (i = 0; i < locations.length; i++) {  
+				    	marker = new google.maps.Marker({
+				        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+				        map: map,
+				        icon: locations[i][3]
+				    	});
+
+						bounds.extend(marker.position);
+
+						google.maps.event.addListener(marker, 'click', (function(marker, i) {
+						    return function() {
+						        infowindow.setContent(locations[i][0]);
+						        infowindow.open(map, marker);
+						    }
+						})(marker, i));
+
+						map.fitBounds(bounds);
+
+					}
+				}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
+
+		</script>
+
+	<?php }
+}
+add_action('wp_footer', 'cdash_single_business_map');
+
+function cdash_info_window() {
+	global $post;
+	$output .= "<div style=\x22width: 200px; height: 150px\x22>";
+	$output .= $location['altname'];
+	$output .= "</div>";
+	return $output;
+}
+
+// add business category and member level slugs as body and post class
+function cdash_add_taxonomy_classes($classes) {
+	global $post;
+	$buscats = get_the_terms($post->ID, 'business_category');
+	if ($buscats) {
+		foreach($buscats as $taxonomy) {
+			$classes[] = $taxonomy->slug;
+		}
+	}
+	$buslevels = get_the_terms($post->ID, 'membership_level');
+	if ($buslevels) {
+		foreach($buslevels as $taxonomy) {
+			$classes[] = $taxonomy->slug;
+		}
+	}
+	return $classes;
+}
+add_filter('post_class', 'cdash_add_taxonomy_classes');
+add_filter('body_class', 'cdash_add_taxonomy_classes');
+
+?>
--- readme.txt
+++ readme.txt
@@ -8,25 +8,21 @@ Stable tag: trunk
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
-Create a directory of businesses.  Specifically designed for chambers of commerce to store information about businesses and 
-display them on a website.
+Create a directory of businesses.  Specifically designed for chambers of commerce to store information about businesses and display them on a website.
 
 == Description ==
-Chamber Dashboard Business Directory is a part of the Chamber Dashboard collection of plugins and themes designed to meet the 
-needs of chambers of commerce.
+Chamber Dashboard Business Directory is a part of the Chamber Dashboard collection of plugins and themes designed to meet the needs of chambers of commerce.
 
-With Chamber Dashboard Business Directory, you can:
+= With Chamber Dashboard Business Directory, you can: =
 *   create a database of the businesses in your chamber
 *   display a business directory on your website
 *   customize what information is displayed about the businesses
 
 You can learn more at [chamberdashboard.com](http://chamberdashboard.com)
 
-For full instructions about how to use the plugin, go to [Chamber Dashboard 
-Documentation](http://chamberdashboard.com/support/documentation)
+For full instructions about how to use the plugin, go to [Chamber Dashboard Documentation](http://chamberdashboard.com/support/documentation)
 
-More features coming soon!  Chamber Dashboard is in the early stages of its development.  By the end of 2014, it will include 
-membership management, event sign-up and management, and much more.
+More features coming soon!  Chamber Dashboard is in the early stages of its development.  By the end of 2014, it will include membership management, event sign-up and management, and much more.
 
 == Installation ==
 = Using The WordPress Dashboard =
@@ -57,19 +53,27 @@ membership management, event sign-up and management, and much more.
 Create a page, and insert the following shortcode:
 [business_directory]
 
-There are lots of options for this shortcode.  For a full description, see [Chamber Dashboard 
-Documentation](http://chamberdashboard.com/document/displaying-business-directory-site/)
+There are lots of options for this shortcode.  For a full description, see [Chamber Dashboard Documentation](http://chamberdashboard.com/document/displaying-business-directory-site/)
 
 = Will it work with my theme? =
 Probably!  It is designed to work with any theme that follows basic WordPress coding practices. 
 
 = I want the plugin to do _____.  Can you make it do that? =
-Probably!  I am definitely interested in making this as useful as possible for chambers of commerce, so please let me know 
-what features you need!  You can use the contact form at [chamberdashboard.com/contact](http://chamberdashboard.com/contact)
+Probably!  I am definitely interested in making this as useful as possible for chambers of commerce, so please let me know what features you need!  You can use the contact form at [chamberdashboard.com/contact](http://chamberdashboard.com/contact)
 
 == Screenshots ==
 1. Entering business information
 
 == Changelog ==
+= 1.2 =
+* Added the ability to create custom fields for businesses
+* Bug fixes
+
+= 1.1 =
+* Added Google map to single business view
+* Added shortcode to create map of all businesses
+* Added category and membership level to body and post classes
+* Bug fixes
+
 = 1.0 =
 * First release
